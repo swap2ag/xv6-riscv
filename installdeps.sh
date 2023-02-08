add61106e4c069e5dc20b5667d3da424898420f6
Swapnil <agarwal.swapnil640@gmail.com>
2023-02-08 20:11:14 +0000

    Add updating of apt repo before installing

--- installdeps.sh
+++ installdeps.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # This was tested on Ubuntu 20.04
-
+sudo apt update -y
 
 # for the GNU toolchain
-sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
+sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev -y
 
 # This is used by GDB so can run a dashboard script
-sudo apt-get install python-dev
+sudo apt-get install python-dev -y
 
 
 
 # The following may not be necessary on 20.04.  But my tests included it.
-sudo apt install ninja-build libglib2.0-dev libpixman-1-dev
+sudo apt install ninja-build libglib2.0-dev libpixman-1-dev -y
 
 
 # This is a useful tool for viewing the configuration of the QEMU-emulated machines
-sudo apt install device-tree-compiler
+sudo apt install device-tree-compiler -y
 
