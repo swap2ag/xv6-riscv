e545c61f32e23b094359f0d2f122d5b3931728d9
Swapnil <agarwal.swapnil640@gmail.com>
2023-02-08 21:21:07 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ mv riscv64-unknown-elf.gcc-12.1.0.tar.gz installation/
 cd installation
 tar -xvzf riscv64-unknown-elf.gcc-12.1.0.tar.gz
 export PATH=$PATH:$SCRIPT_DIR/installation
+echo "export PATH=$PATH:$SCRIPT_DIR/installation" >> ~/.bashrc
+
+# git clone git clone https://github.com/qemu/qemu --depth=1
+cd qemu
+git checkout v5.0.0
+./configure --target-list=riscv64-softmmu
+make -j $(nproc)
+sudo make install
